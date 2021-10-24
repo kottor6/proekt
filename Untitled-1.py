5e0b5dafb08990e1a607b6741f13ed3c905fa823
kottor6 <91839489+kottor6@users.noreply.github.com>
2021-10-24 19:08:53 +0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,5 @@
-from PyQt.QtCore import Qt
-from PyQt.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel)
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QButtonGroup, QGroupBox, QRadioButton, QPushButton, QLabel)
 app = QApplication([])
 window = QWidget()
 
@@ -12,14 +12,20 @@ rbtn_2 = QRadioButton('993')
 rbtn_3 = QRadioButton('6')
 rbtn_4 = QRadioButton('1003')
 
-layout_ans1 = QHBoxLayout
-layout_ans2 = QVBoxLayout
-layout_ans3 = QVBoxLayout
+RadioGroup = QButtonGroup()
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
 
-layout_ans2.addWdget(rbtn_1)
-layout_ans2.addWdget(rbtn_2)
-layout_ans3.addWdget(rbtn_3)
-layout_ans3.addWdget(rbtn_4)
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_4)
 
 layout_ans1.addLayout(layout_ans2)
 layout_ans1.addLayout(layout_ans3)
@@ -45,6 +51,30 @@ layout_card.addStretch(1)
 layout_card.addLayout(layout_line3, stretch = 1)
 layout_card.addStretch(1)
 
+def show_result():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+
+
+def show_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False)
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True)
+
+def test():
+    if 'Ответить' == btn_OK.text():
+        show_result()
+    else:
+        show_question()
+
+btn_OK.clicked.conect(test)
 window.setLayout(layout_card)
 window.show()
 app.exec_()
