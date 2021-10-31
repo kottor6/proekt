fb1b453758920cdc12a328f3bfa9e572292c77e2
kottor6 <91839489+kottor6@users.noreply.github.com>
2021-10-31 19:29:39 +0200

    Add files via upload

--- memcard.py
+++ memcard.py
@@ -1,29 +1,48 @@
 from PyQt5.QtCore import Qt
+from random import shuffle                                                                                                                                                                                                                                                               
 from PyQt5.QtWidgets import (
         QApplication, QWidget, 
         QHBoxLayout, QVBoxLayout, 
-        QGroupBox, QRadioButton,  
+        QGroupBox, QButtonGroup, QRadioButton,  
         QPushButton, QLabel)
  
+
+class Question():
+    def __init__(self, question, right_answer,wrong1,wrong2,wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+
+
+
+
 app = QApplication([])
  
-# Создаем панель вопроса
-btn_OK = QPushButton('Ответить')
-lb_Question = QLabel('Самый сложный вопрос в мире!')
+btn_OK = QPushButton('Ответить') 
+lb_Question = QLabel('Сколько весит слон?') 
  
 RadioGroupBox = QGroupBox("Варианты ответов")
  
-rbtn_1 = QRadioButton('Вариант 1')
-rbtn_2 = QRadioButton('Вариант 2')
-rbtn_3 = QRadioButton('Вариант 3')
-rbtn_4 = QRadioButton('Вариант 4')
+rbtn_1 = QRadioButton('да')
+rbtn_2 = QRadioButton('нет')
+rbtn_3 = QRadioButton('незнаю')
+rbtn_4 = QRadioButton('Вариант4')
+ 
+RadioGroup = QButtonGroup() 
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
  
 layout_ans1 = QHBoxLayout()   
 layout_ans2 = QVBoxLayout()
 layout_ans3 = QVBoxLayout()
-layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_1) 
 layout_ans2.addWidget(rbtn_2)
-layout_ans3.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans3.addWidget(rbtn_3) 
 layout_ans3.addWidget(rbtn_4)
  
 layout_ans1.addLayout(layout_ans2)
@@ -31,32 +50,27 @@ layout_ans1.addLayout(layout_ans3)
  
 RadioGroupBox.setLayout(layout_ans1)
  
-# Создаем панель результата
 AnsGroupBox = QGroupBox("Результат теста")
-lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
-lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
- 
+lb_Result = QLabel('прав ты или нет?') 
+lb_Correct = QLabel('ответ будет тут!')
 layout_res = QVBoxLayout()
 layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
 layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
 AnsGroupBox.setLayout(layout_res)
  
-# Размещаем все виджеты в окне:
-layout_line1 = QHBoxLayout() # вопрос
-layout_line2 = QHBoxLayout() # варианты ответов или результат теста
-layout_line3 = QHBoxLayout() # кнопка "Ответить"
+layout_line1 = QHBoxLayout() 
+layout_line2 = QHBoxLayout() 
+layout_line3 = QHBoxLayout() 
  
 layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
-# Размещаем в одной строке обе панели, одна из них будет скрываться, другая показываться:
 layout_line2.addWidget(RadioGroupBox)   
 layout_line2.addWidget(AnsGroupBox)  
-RadioGroupBox.hide() # эту панель мы уже видели, скроем, посмотрим, как получилась панель с ответом
+AnsGroupBox.hide()
  
 layout_line3.addStretch(1)
 layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
 layout_line3.addStretch(1)
  
-# Теперь созданные строки разместим друг под другой:
 layout_card = QVBoxLayout()
  
 layout_card.addLayout(layout_line1, stretch=2)
@@ -66,9 +80,84 @@ layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
 layout_card.setSpacing(5) # пробелы между содержимым
  
-window = QWidget()
-window.setLayout(layout_card)
-window.setWindowTitle('Memory Card')
-window.show()
+# ----------------------------------------------------------
+# Виджеты и макеты созданы, далее - функции:
+# ----------------------------------------------------------
  
+def show_result():
+    ''' показать панель ответов '''
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+ 
+def show_question():
+    ''' показать панель вопросов '''
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False) # сняли ограничения, чтобы можно было сбросить выбор радиокнопки
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True) # вернули ограничения, теперь только одна радиокнопка может быть выбрана
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4] 
+
+
+
+def ask(q: Question):
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_Question.setText(q.question)
+    lb_Question.setText(q.right_answer)
+    show_question()
+
+def next_question():
+    window.question +=1
+    if window.question >= len(Question_list):
+        window.question = 0
+    q = question_list[window.question]
+    ask(q)
+
+
+def show_correct(res):
+    lb_Result.setText(res)
+    show_result()
+
+def test():
+    ''' временная функция, которая позволяет нажатием на кнопку вызывать по очереди
+    show_result() либо show_question() '''
+    if answers[0].isChecked():
+        show_correct('Правильно')
+    elif answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+         show_correct('Неверно')
+
+
+def click_OK():
+    if btn_OK.text() == 'Ответить':
+        test()
+    else:
+        next_question()
+
+
+
+
+
+
+    
+q = Question('Сколько весит слон?','да', 'нет', 'незнаю','Вариант4')
+question_list = []
+question_list.append(q)
+question_list.append(Question('1000-7', '993', '998', '4', '21'))
+
+window = QWidget()
+window.question = -1
+window.setLayout(layout_card)
+window.setWindowTitle('Memo Card')
+btn_OK.clicked.connect(test) # проверяем, что панель ответов показывается при нажатии на кнопку
+window.show()
 app.exec()
+
